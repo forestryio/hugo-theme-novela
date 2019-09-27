87d529d3ced832c198e9e83ec8ab081dbeeca28c
mountainbug95 <tannersmithtn95@gmail.com>
2019-09-27 16:15:49 -0400

    Add extra mobile support for the list layout

--- assets/js/toggleLayout.js
+++ assets/js/toggleLayout.js
@@ -1,10 +1,26 @@
 document.getElementById("btnGrid").addEventListener("click", makeGrid);
 document.getElementById("btnRows").addEventListener("click", makeRows);
+window.addEventListener("resize", adjustListAtMobile);
+
+let listRemoveWidth = window.matchMedia("(max-width: 735px)");
+let listAddWidth = window.matchMedia("(min-width: 735px)");
+let isList = false;
+
+function adjustListAtMobile(){
+    if (listRemoveWidth.matches) {
+        document.getElementById("articlesList").classList.remove("articles-list-container-alt");
+    }
+    else if (listAddWidth.matches && isList === true) {
+        document.getElementById("articlesList").classList.add("articles-list-container-alt");
+    }
+}
 
 function makeGrid(){
     document.getElementById("articlesList").classList.remove("articles-list-container-alt");
+    isList = false;
 }
 
 function makeRows(){
     document.getElementById("articlesList").classList.add("articles-list-container-alt");
+    isList = true;
 }
