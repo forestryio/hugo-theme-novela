93d94f829f90c63e2825a8f6eb68d95d2252eb92
mountainbug95 <tannersmithtn95@gmail.com>
2019-10-01 16:03:33 -0400

    Update layout icon colors when inactive

--- assets/js/toggleLayout.js
+++ assets/js/toggleLayout.js
@@ -2,6 +2,8 @@ document.getElementById("btnGrid").addEventListener("click", makeGrid);
 document.getElementById("btnRows").addEventListener("click", makeRows);
 window.addEventListener("resize", adjustListAtMobile);
 
+document.getElementById("rows").style.fill = "var(--grey)";
+
 let listRemoveWidth = window.matchMedia("(max-width: 735px)");
 let listAddWidth = window.matchMedia("(min-width: 735px)");
 let isList = false;
@@ -17,10 +19,14 @@ function adjustListAtMobile(){
 
 function makeGrid(){
     document.getElementById("articlesList").classList.remove("articles-list-container-alt");
+    document.getElementById("tiles").style.fill = "var(--primary)";
+    document.getElementById("rows").style.fill = "var(--grey)";
     isList = false;
 }
 
 function makeRows(){
     document.getElementById("articlesList").classList.add("articles-list-container-alt");
+    document.getElementById("rows").style.fill = "var(--primary)";
+    document.getElementById("tiles").style.fill = "var(--grey)";
     isList = true;
 }
