1e44ed00e6a4ab334f4994588dade00fa92e5688
mountainbug95 <tannersmithtn95@gmail.com>
2019-10-04 15:02:27 -0400

    Add color toggle animation, fixes #25

--- assets/js/toggleColors.js
+++ assets/js/toggleColors.js
@@ -1,15 +1,63 @@
 document.getElementById("themeColorButton").addEventListener("click", toggleColors);
 
 let bodyElement = document.getElementsByTagName("BODY")[0];
+let moonOrSun = document.getElementById("moonOrSun");
+let sunRays = document.getElementById("sunRays");
+let moonMask = document.getElementById("moonMask");
+
+initAnimation();
+
+function initAnimation(){
+    if (localStorage.getItem("isLight") === 'true'){
+        moonMask.style.top = "-8px";
+        moonMask.style.right = "-1px";
+        moonOrSun.style.transform = "scale(1)"
+        sunRays.style.transform = "scale(0.1)"
+    }
+    else{
+        moonMask.style.top = "-25px";
+        moonMask.style.right = "-15px";
+        moonOrSun.style.transform = "scale(0.5)"
+        sunRays.style.transform = "scale(0.6)"
+    }
+}
 
 function toggleColors(){
     if (localStorage.getItem("isLight") === 'true'){
         makeDark();
 
+        moonMask.classList.add("mask-to-sun-animation");
+        moonOrSun.classList.add("to-sun-animation");
+        sunRays.classList.add("expand-rays");
+
+        setTimeout(function(){
+            sunRays.classList.remove("expand-rays");
+        }, 500);
+        setTimeout(function(){
+            moonMask.classList.remove("mask-to-sun-animation");
+            moonOrSun.classList.remove("to-sun-animation");
+            initAnimation();
+        }, 400);
+
         localStorage.setItem("isLight", 'false');
     }
     else{
         makeLight();
+
+        moonMask.classList.add("mask-to-moon-animation");
+        moonOrSun.classList.add("to-moon-animation");
+        sunRays.classList.add("contract-rays");
+
+
+        setTimeout(function(){
+            sunRays.classList.remove("contract-rays");
+        }, 500);
+        setTimeout(function(){
+            moonMask.classList.remove("mask-to-moon-animation");
+            moonOrSun.classList.remove("to-moon-animation");
+            sunRays.classList.remove("contract-rays");
+            initAnimation();
+        }, 400);
 
         localStorage.setItem("isLight", 'true');
     }
